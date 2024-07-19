3702e7a5b2937390ec9ec33eced5f6ab6616993f
Pawanpreetkaur963 <pawanpreetkaur7@gmail.com>
2024-07-19 16:01:44 -0500

    out putting list of posts

--- react-crash-course/src/App.jsx
+++ react-crash-course/src/App.jsx
@@ -1,10 +1,29 @@
+import { useState } from 'react';
+
 import PostsList from './components/postsList';
+import MainHeader from './components/mainheader';
 
 function App() {
+  const [modalIsVisible, setModalIsVisible] = useState(false);
+
+  function showModalHandler() {
+    setModalIsVisible(true);
+  }
+
+  function hideModalHandler() {
+    setModalIsVisible(false);
+  }
+
   return (
-    <main>
-      <PostsList />
-    </main>
+    <>
+      <MainHeader onCreatePost={showModalHandler} />
+      <main>
+        <PostsList
+          isPosting={modalIsVisible}
+          onStopPosting={hideModalHandler}
+        />
+      </main>
+    </>
   );
 }
 
