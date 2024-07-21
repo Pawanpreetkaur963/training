abf06d5fa820134a15ba87dd9d0c4f9db86b54df
Pawanpreetkaur963 <pawanpreetkaur7@gmail.com>
2024-07-21 03:00:51 -0500

    adding routes

--- react-crash-course/src/App.jsx
+++ react-crash-course/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-
+import { Link } from 'react-router-dom';
 import PostsList from './components/postsList';
 import MainHeader from './components/mainheader';
 
--- react-crash-course/src/components/newpost.jsx
+++ react-crash-course/src/components/newpost.jsx
@@ -1,6 +1,6 @@
 
 import { useState } from 'react';
-
+import { useNavigate } from 'react-router-dom';
 import classes from './newpost.module.css';
 
 function NewPost({ onCancel, onAddPost }) {
